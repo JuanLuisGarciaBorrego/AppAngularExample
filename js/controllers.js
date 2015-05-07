ff1ae6afaa9a5c49d7132d8cc6268b28fead5837
Juan Luis García Borrego <juanluisgarciaborrego@gmail.com>
2015-05-07 09:05:14 +0200

    resource api posts and users

--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,11 @@
 app.controller("MainController",function($scope, $resource){
 
+    Post = $resource("http://jsonplaceholder.typicode.com/posts/:id",{id: "@id"});
+    User = $resource("http://jsonplaceholder.typicode.com/users/:id",{id: "@id"});
+
+    $scope.posts= Post.query();
+    $scope.users= User.query();
+
+    //query() -> GET/posts = array de posts
+
 });
